ca5d22c6044ebd70a5f80ff8458e5ffcf1088f96
葛万平 <7800789+ge_wanping@user.noreply.gitee.com>
2020-08-16 23:13:52 +0800

    添加了QQview

---
+++ gitTest/gitTest/qqqq.h
@@ -0,0 +1,17 @@
+//
+//  qqqq.h
+//  gitTest
+//
+//  Created by bjjc on 2020/8/16.
+//  Copyright © 2020 bjjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface qqqq : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ gitTest/gitTest/qqqq.m
@@ -0,0 +1,21 @@
+//
+//  qqqq.m
+//  gitTest
+//
+//  Created by bjjc on 2020/8/16.
+//  Copyright © 2020 bjjc. All rights reserved.
+//
+
+#import "qqqq.h"
+
+@implementation qqqq
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
