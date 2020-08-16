2ec5b12d8c889748850fefe3cd4931492ccf3345
葛万平 <7800789+ge_wanping@user.noreply.gitee.com>
2020-08-16 19:13:03 +0800

    project init

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by bjjc on 2020/8/16.
+//  Copyright © 2020 bjjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ gitTest/gitTest/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  gitTest
+//
+//  Created by bjjc on 2020/8/16.
+//  Copyright © 2020 bjjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
